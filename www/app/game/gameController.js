583d49a54bf18327243a0c2b9ee8fcc02e5033fe
Zviad Sichinava <zviadsichinava@gmail.com>
2016-01-22 14:34:15 +0400

    Added ripple animation && changed font

--- www/app/game/gameController.js
+++ www/app/game/gameController.js
@@ -1,7 +1,8 @@
-app.controller('gameController', ['$scope', '$timeout', '$state', '$stateParams', 'Game', 'Storage',
-function($scope, $timeout, $state, $stateParams, Game, Storage){
+app.controller('gameController', ['$scope', '$timeout', '$state', '$stateParams', 'Game', 'Storage', '$ionicGesture',
+function($scope, $timeout, $state, $stateParams, Game, Storage, $ionicGesture){
 
 	var self = this;
+	var gameboardWrapper = angular.element(document.querySelector("#gameboardWrapper"));
 
 	/**** GAMEPLAY OBJECT ****/
 	function GamePlay(){
@@ -48,29 +49,62 @@ function($scope, $timeout, $state, $stateParams, Game, Storage){
 
 	// GAME OVER
 	GamePlay.prototype.gameOver = function(){
-		TweenMax.set(preloader, {width: "0%"});
+		/*TweenMax.set(preloader, {width: "0%"});
 		if (this.timerAnimation) {
 			this.timerAnimation.kill();
 		}
 		
 		console.log('GAME OVER');
 		Game.setEarnedScore(this.level.id, this.currentScore);
-		$state.go("menu");
+		$state.go("menu");*/
 	};
 
 	// SELEC ANSWER
-	GamePlay.prototype.selectAnswer = function(index){
+	GamePlay.prototype.selectAnswer = function(index, event){
 		TweenMax.set('#block-0' + index, {backgroundColor: 'rgba(255,255,255,0.07)'});
 		TweenMax.set('#block-1' + index, {backgroundColor: 'transparent'});
 		if (self.currentQuestion[index].value > self.currentQuestion[index === 0 ? 1 : 0].value) {
 			this.currentScore ++;
 			this.nextQuestion();
+			gamePlay.rippleAnimation(event.pageX, event.pageY, 0.5, 60, "#bedb39");
 			TweenMax.from('#block-' + index, 0.3, {backgroundColor: '#bedb39'});
 		}else{
 			this.gameOver();
+			gamePlay.rippleAnimation(event.pageX, event.pageY, 0.5, 60, "red");
 			TweenMax.from('#block-' + index, 0.3, {backgroundColor: 'red'});
 		}
 	};
+
+	GamePlay.prototype.rippleAnimation = function(x, y, timing, scale, color){
+		var ripple 		  = document.querySelectorAll('.js-ripple'),
+			rippleContent = document.getElementById('ripple-content'),
+			tl            = new TimelineMax();
+			x             = x,
+			y             = y,
+			w             = rippleContent.offsetWidth,
+			h             = rippleContent.offsetHeight;
+
+		TweenMax.set(rippleContent, {
+			x: x - w / 2,
+			y: y - h / 2,
+		});
+
+		tl.fromTo(ripple, timing, {
+			fill: color,
+			x: w / 2,
+			y: h / 2,
+			transformOrigin: '50% 50%',
+			scale: 0,
+			opacity: 1,
+			ease: Power4.easeOut
+		},{
+			scale: scale,
+			opacity: 0
+		});
+	};
+
+
+
 	/**** GAMEPLAY OBJECT END ****/
 
 
@@ -83,8 +117,8 @@ function($scope, $timeout, $state, $stateParams, Game, Storage){
 	});
 
 	// ANSWER QEUSTION
-	$scope.selectOpt = function(index){
-		gamePlay.selectAnswer(index);
+	$scope.selectOpt = function(index, event){
+		gamePlay.selectAnswer(index, event);
 	};
 
 }]);
